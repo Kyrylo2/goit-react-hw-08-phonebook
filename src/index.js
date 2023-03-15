ef71e757c77ceb08259110aa2274138fe1e660ad
Kyrylo <savchenko.kyrylo@gmail.com>
2023-03-15 21:54:23 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={<Spinner />} persistor={persistor}>
-        <BrowserRouter>
+        <BrowserRouter basename="/goit-react-hw-08-movies">
           <App />
         </BrowserRouter>
       </PersistGate>
