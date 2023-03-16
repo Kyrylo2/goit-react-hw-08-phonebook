0673819a0c2d8fe1eaaade065c107011a3af3075
Kyrylo <savchenko.kyrylo@gmail.com>
2023-03-16 23:56:19 +0200

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,85 +1,83 @@
 import { useState } from 'react';
-
-import { FormControl } from '@mui/material';
+import { FormControl, Typography } from '@mui/material';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
-
 import { useAddContactMutation } from 'redux/contactsApi';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+
+const phoneRegExp = /^\+380\d{9}$/;
+
+const validationSchema = Yup.object().shape({
+  name: Yup.string().required('Name is required'),
+  phone: Yup.string()
+    .matches(phoneRegExp, 'Phone number must be a valid Ukrainian phone number')
+    .required('Phone number is required'),
+});
 
 export default function ContactsForm() {
-  const [name, setName] = useState('');
-  const [number, setNumber] = useState('');
-
   const [addContact] = useAddContactMutation();
 
-  const handleStateChange = e => {
-    const { name, value } = e.target;
-
-    switch (name) {
-      case 'name':
-        setName(value);
-        break;
-
-      case 'phone':
-        setNumber(value);
-        break;
-
-      default:
-        return;
-    }
+  const initialValues = {
+    name: '',
+    phone: '',
   };
 
-  const resetForm = () => {
-    setName('');
-    setNumber('');
-  };
-
-  const handleSubmit = async e => {
-    e.preventDefault();
-
+  const handleSubmit = async (values, { resetForm }) => {
     const formObj = {
-      name,
-      number,
+      name: values.name,
+      number: values.phone,
     };
     await addContact(formObj).unwrap();
-
-    // dispatch(addContact(formObj));
-
-    // onSubmit(name, number, nanoid());
     resetForm();
   };
 
   return (
-    <FormControl
-      component="form"
-      noValidate
-      autoComplete="off"
+    <Formik
+      initialValues={initialValues}
       onSubmit={handleSubmit}
-      onChange={handleStateChange}
-      sx={{
-        minWidth: '350px',
-        maxWidth: '500px',
-        gap: '10px',
-        margin: '0 auto',
-      }}
+      validationSchema={validationSchema}
     >
-      <TextField
-        id="outlined-search"
-        label="Full Name"
-        name="name"
-        type="text"
-        value={name}
-      />
-      <TextField
-        id="outlined-search"
-        label="Phone"
-        name="phone"
-        type="tel"
-        value={number}
-      />
-      <Button variant="contained" size="small" type="submit">
-        Add contact
-      </Button>
-    </FormControl>
+      {({ errors, touched }) => (
+        <Form>
+          <FormControl
+            noValidate
+            autoComplete="off"
+            sx={{
+              minWidth: '350px',
+              maxWidth: '500px',
+              gap: '10px',
+              margin: '0 auto',
+            }}
+          >
+            <Field
+              as={TextField}
+              id="outlined-search"
+              label="Full Name"
+              name="name"
+              type="text"
+              error={touched.name && !!errors.name}
+              helperText={touched.name && errors.name}
+            />
+            <Field
+              as={TextField}
+              id="outlined-search"
+              label="Phone"
+              name="phone"
+              type="tel"
+              error={touched.phone && !!errors.phone}
+              helperText={touched.phone && errors.phone}
+            />
+            <Typography variant="caption" color="text.secondary">
+              Phone number must be a valid Ukrainian phone number
+              (+380XXXXXXXXX)
+            </Typography>
+            <Button variant="contained" size="small" type="submit">
+              Add contact
+            </Button>
+          </FormControl>
+        </Form>
+      )}
+    </Formik>
   );
 }
